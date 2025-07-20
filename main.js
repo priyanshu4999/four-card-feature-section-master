ae948f0fadd9b0cab1ba7e796fbd160136d556d0
priyanshu4999 <jain.priyanshu4999@gmail.com>
2025-07-20 20:17:26 +0530

    finishing touches

--- main.js
+++ main.js
@@ -33,31 +33,31 @@ app.innerHTML = `
     <article class="container">
         <section class="hero">
             <h1 class="hero__primary text-extralight-xl">${data.headlinerPrimary}</h1>
-            <h2 class="hero__secondary text-semibold-xl"">${data.headlinerSecondary}</h2>
+            <h2 class="hero__secondary text-semibold-xl">${data.headlinerSecondary}</h2>
             <p class="hero__intro text-regular-s">${data.introText}</p>
         </section>
         <section class="card-grid">
             <div class="card card--cyan">
                 <h3 class="text-semibold-m">${data.cards.supervisor.title}</h3>
                 <span class="text-regular-xs">${data.cards.supervisor.description}</span>
-                <figure><img src="${data.cards.supervisor.img}"/></figure>
+                <figure><img src="${data.cards.supervisor.img}" alt="${data.cards.supervisor.title}"/></figure>
             </div>
             <div class="card-grid__central"> 
                 <div class="card card--red">
                     <h3 class="text-semibold-m">${data.cards.teamBuilder.title}</h3>
                     <span class="text-regular-xs">${data.cards.teamBuilder.description}</span>
-                    <figure><img src="${data.cards.teamBuilder.img}"/></figure>
+                    <figure><img src="${data.cards.teamBuilder.img}" alt="${data.cards.teamBuilder.title}"/></figure>
                     </div>
                 <div class="card  card--orange">
                     <h3 class="text-semibold-m">${data.cards.karma.title}</h3>
                     <span class="text-regular-xs">${data.cards.karma.description}</span>
-                    <figure><img src="${data.cards.karma.img}"/></figure>
+                    <figure><img src="${data.cards.karma.img}" alt="${data.cards.karma.title}"/></figure>
                 </div>
             </div>
             <div class="card card--blue">
                 <h3 class="text-semibold-m">${data.cards.calculator.title}</h3>
                 <span class="text-regular-xs">${data.cards.calculator.description}</span>
-                <figure><img src="${data.cards.calculator.img}"/></figure>
+                <figure><img src="${data.cards.calculator.img}" alt="${data.cards.calculator.title}"/></figure>
             </div>
         
         </section>
